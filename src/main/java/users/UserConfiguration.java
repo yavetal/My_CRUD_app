9b35f2b9be7d424f3c64af362260436efde511ae
Vitaliy Yanitskiy <yvetal@gmail.com>
2023-05-27 23:59:24 +0300

    add CR\UD controller delete method get.ById(id) changes files UserConfiguration and application.properties

--- src/main/java/users/UserConfiguration.java
+++ src/main/java/users/UserConfiguration.java
@@ -1,5 +1,6 @@
 package users;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -8,13 +9,25 @@ import javax.sql.DataSource;
 
 @Configuration
 public class UserConfiguration {
+    @Value("${database.driverClassName}")
+    private String driverClassName;
+
+    @Value("${database.url}")
+    private String url;
+
+    @Value("${database.username}")
+    private String username;
+
+    @Value("${database.password}")
+    private String password;
+
     @Bean
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://localhost:3306/new");
-        dataSource.setUsername("root");
-        dataSource.setPassword("AwerS2013");
+        dataSource.setDriverClassName(driverClassName);
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
         return dataSource;
     }
 }
--- src/main/java/users/UserController.java
+++ src/main/java/users/UserController.java
@@ -3,49 +3,56 @@ package users;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
 import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/read")
 public class UserController {
 
-    private final UserRepository userRepository ;
+    private final UserRepository userRepository;
+
     public UserController(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
     @GetMapping("/all")
-    public String all(Model model){
+    public String all(Model model) {
         List<Users> all = userRepository.findAll();
         System.out.println(all);
         model.addAttribute("users", all);
         return "all";
     }
+
     @GetMapping("/{id}")
     @Transactional
-    public  String  id (@PathVariable("id") Long id, Model model){
-        Users user = userRepository.getById(id);
+    public String id(@PathVariable("id") Long id, Model model) {
+        Optional<Users> optionalUser = userRepository.findById(id);
+        Users user = optionalUser.get();
         System.out.println(user);
-       model.addAttribute("user", user);
+        model.addAttribute("user", user);
         return "id";
     }
+
     @GetMapping("/new")
-    public String newUser(Model model){
+    public String newUser(Model model) {
         model.addAttribute("user", new Users());
         return "new";
     }
+
     @PostMapping()
-    public String createNewUser(@ModelAttribute("user") Users user){
+    public String createNewUser(@ModelAttribute("user") Users user) {
         userRepository.save(user);
         return "redirect:/read/all";
     }
 
     @GetMapping("/{id}/edit")
     @Transactional
-    public String editUser(@PathVariable("id") Long id, Model model){
-        model.addAttribute("user", userRepository.getById(id));
-        System.out.println(userRepository.getById(id));
+    public String editUser(@PathVariable("id") Long id, Model model) {
+        model.addAttribute("user", userRepository.findById(id).get());
+        System.out.println(userRepository.findById(id).get());
         return "edit";
     }
 
@@ -58,8 +65,9 @@ public class UserController {
 
     @PostMapping("/delite/{id}")
     public String deliteUserr(@PathVariable("id") Long id) {
+
         System.out.println(id.toString());
-        userRepository.delete(userRepository.getById(id));
+        userRepository.delete(userRepository.findById(id).get());
         return "redirect:/read/all";
     }
 }
